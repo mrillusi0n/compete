bc41c1eee5cf8d16f584d327c6190f3ab0e5d903
Nikhil CSB <csbnikhil@gmail.com>
2020-08-27 12:45:36 +0530

    figure out

--- humanity.py
+++ humanity.py
@@ -7,28 +7,7 @@ If there is no matching substring, output No Match!.
 '''
 
 def check(p, v):
-	i = 0
-	j = -1
-	k = len(v) - 2
-	flag = 'No Match!'
-
-	while i <= len(p) - len(v):
-		error = 0
-		j += 1
-		k += 1
-		while error < 2 and j < k:
-			if p[j] != v[j]:
-				error += 1
-			if p[k] != v[k]:
-				error += 1
-			k -= 1
-			j += 1
-		if error < 2:
-			print(i, end=' ')
-			flag = ''
-		i += 1
-
-	print()
+    pass
 
 if __name__ == '__main__':
-	check('abbab', 'ba')
+    check('abbab', 'ba')
