63da01161b3e5409e8153f642abbb10c35475d15
Nikhil CSB <csbnikhil@gmail.com>
2020-11-29 20:16:43 +0530

    convert to spaces

--- in_order_successor.py
+++ in_order_successor.py
@@ -1,79 +1,79 @@
 class Node:
-	def __init__(self, key):
-		self.key = key 
-		self.left = None
-		self.right = None
-		self.parent = None
+    def __init__(self, key):
+        self.key = key 
+        self.left = None
+        self.right = None
+        self.parent = None
 
-	def __repr__(self):
-		return 'Node: {}'.format(self.key)
+    def __repr__(self):
+        return 'Node: {}'.format(self.key)
 
 class BinarySearchTree:
 
-	def __init__(self):
-		self.root = None 
+    def __init__(self):
+        self.root = None 
 
-	def find_in_order_successor(self, inputNode):
-		currTraveller = self.root
-		currKey = self.root.key
-		currSuccessor = None
-		
-		key = inputNode.key
+    def find_in_order_successor(self, inputNode):
+        currTraveller = self.root
+        currKey = self.root.key
+        currSuccessor = None
+        
+        key = inputNode.key
 
-		while currTraveller:
-			if currTraveller.key > key:
-				if currTraveller.key <= currKey:
-					currSuccessor = currTraveller
-					currKey = currTraveller.key
-				currTraveller = currTraveller.left
-			else:
-				currTraveller = currTraveller.right
-				currKey = currTraveller.key if currTraveller else currKey
+        while currTraveller:
+            if currTraveller.key > key:
+                if currTraveller.key <= currKey:
+                    currSuccessor = currTraveller
+                    currKey = currTraveller.key
+                currTraveller = currTraveller.left
+            else:
+                currTraveller = currTraveller.right
+                currKey = currTraveller.key if currTraveller else currKey
 
-		
-		return currSuccessor
-		
+        
+        return currSuccessor
+        
 
 
-	def insert(self, key):
-		
-		if (self.root is None):
-			self.root = Node(key)
-			return
-				
-		currentNode = self.root
-		newNode = Node(key)
-		while(currentNode is not None):
-			
-			if(key < currentNode.key):
-				if(currentNode.left is None):
-					currentNode.left = newNode;
-					newNode.parent = currentNode;
-					break
-				else:
-					currentNode = currentNode.left;
-			else:
-				if(currentNode.right is None):
-					currentNode.right = newNode;
-					newNode.parent = currentNode;
-					break
-				else:
-					currentNode = currentNode.right;
+    def insert(self, key):
+        
+        if (self.root is None):
+            self.root = Node(key)
+            return
+                
+        currentNode = self.root
+        newNode = Node(key)
+        while(currentNode is not None):
+            
+            if(key < currentNode.key):
+                if(currentNode.left is None):
+                    currentNode.left = newNode;
+                    newNode.parent = currentNode;
+                    break
+                else:
+                    currentNode = currentNode.left;
+            else:
+                if(currentNode.right is None):
+                    currentNode.right = newNode;
+                    newNode.parent = currentNode;
+                    break
+                else:
+                    currentNode = currentNode.right;
 
-	def getNodeByKey(self, key):
-		
-		currentNode = self.root
-		while(currentNode is not None):
-			if(key == currentNode.key):
-				return currentNode
-				
-			if(key < currentNode.key):
-				currentNode = currentNode.left
-			else:
-				currentNode = currentNode.right
-				
-		return None
-				
+    def getNodeByKey(self, key):
+        
+        currentNode = self.root
+        while(currentNode is not None):
+            if(key == currentNode.key):
+                return currentNode
+                
+            if(key < currentNode.key):
+                currentNode = currentNode.left
+            else:
+                currentNode = currentNode.right
+                
+        return None
+                
 
 bst     = BinarySearchTree()
 bst.insert(20)
@@ -82,15 +82,15 @@ bst.insert(25);
 bst.insert(5);
 bst.insert(12);
 bst.insert(11); 
-bst.insert(14);	 
+bst.insert(14);     
 
-test = bst.getNodeByKey(9)
+test = bst.getNodeByKey(12)
 
 succ = bst.find_in_order_successor(test)
 
 if succ is not None:
-		print ("\nInorder Successor of %d is %d " \
-						%(test.key , succ.key))
+    print ("\nInorder Successor of %d is %d " \
+                    %(test.key , succ.key))
 else:
-		print ("\nInorder Successor doesn't exist")
+    print ("\nInorder Successor doesn't exist")
 
