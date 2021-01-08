cd50a87a1f34d38b5d6c0655d28eaaac840edfa5
Nikhil CSB <csbnikhil@gmail.com>
2021-01-09 02:48:43 +0530

    a more elegant way

--- red_join.py
+++ red_join.py
@@ -3,9 +3,13 @@
 from functools import reduce, partial
 
 def join(delim, itr):
-    return reduce(lambda acc, item: acc + delim + item, itr)
+    return reduce(lambda acc, item: f'{acc}{delim}{item}', itr)
 
 def join(delim, itr):
     return reduce(partial(str.format, '{1}{0}{2}', delim), itr)
+
+def join(delim, itr):
+    # from 07734willy
+    return reduce(f'{{}}{delim}{{}}'.format, itr)
 
 print(join('*', 'string'))
