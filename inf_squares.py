736734a1e58f1c3d4ec49de14a7b5afcdb0d44fa
Nikhil CSB <csbnikhil@gmail.com>
2021-01-12 21:48:24 +0530

    powers of 2 != raised to 2's power

--- inf_squares.py
+++ inf_squares.py
@@ -1,9 +1,10 @@
 # return a generator of square numbers
 
 from itertools import count
+from functools import partial
 
 def get_squares():
-    return map(int(2).__pow__, count())
+    return map(partial(pow, exp=2), count())
 
 squares = get_squares()
 
