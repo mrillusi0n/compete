4704e5c4ec717034963fadbfa5c57ba4b7003956
Nikhil CSB <csbnikhil@gmail.com>
2021-02-03 01:47:11 +0530

    cap on

--- curry.py
+++ curry.py
@@ -3,17 +3,16 @@ from functools import partial
 def avg_three(a, b, c):
     return (a + b + c) / 3
 
-def cap(n, s=[avg_three]):
-    if len(s) == 3:
-        return s[-1]()
-    s.append(partial(s[-1], n))
-    return cap
+cap = partial(partial, partial, avg_three)
 
 def curried_avg(n):
-    return lambda x: lambda y: (n + x + y) / 3
+    return lambda x: lambda y: avg_three(x, y, n)
 
 # print(cap(5)(4)(3))
 # print(partial(partial(partial(avg_three, 5), 4), 3)())
 # print(partial(partial(avg_three, 5), 4)(3))
-print(cap(5)(4)(3))
+# print(partial(p_func, 4)(3))
+# print(q_func(3))
 
+print(cap(5)(4)(3))
+# print(cap(5))
